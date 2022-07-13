a07356d6844229448eb20cf029dcfd5abdb11972
MyUsername <romamargine@gmail.com>
2022-07-13 20:25:45 +0200

    Add Fix old() in admin\create

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -26,7 +26,7 @@
             <select id="category_id" class="form-control" name="category_id">
                 <option value="">Select a category</option>
                 @foreach ($categories as $category)
-                <option value="{{$category->id}}"  >{{$category->name}}</option>
+                <option value="{{$category->id}}"  {{ $category->id == old('category_id') ? 'selected' : '' }}>{{$category->name}}</option>
                 @endforeach
             </select>
         </div>
